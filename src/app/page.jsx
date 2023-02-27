8f04a17c6a62d429d653537d884321648be7ff83
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-27 11:40:50 -0500

    successful API response via axios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,21 @@
 
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://api-nba-v1.p.rapidapi.com/standings',
+  params: {league: 'standard', season: '2022'},
+  headers: {
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': process.env.API_HOST
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 export default function Home() {
   return (
