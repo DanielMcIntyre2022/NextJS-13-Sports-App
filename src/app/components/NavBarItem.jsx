b20fd444b8152d4d430e72f376f4f281491c6643
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-27 10:44:37 -0500

    utilized title and param props to link user to page based on thier navbar selection

--- src/app/components/NavBarItem.jsx
+++ src/app/components/NavBarItem.jsx
@@ -1,8 +1,19 @@
+'use client'
+
+import Link from "next/link";
+import { useSearchParams } from 'next/navigation';
 
 function NavBarItem({title, param}) {
+
+const searchParams = useSearchParams();
+
   return (
     <div>
-      {title}
+      <Link 
+      className="m-4 p-2"
+      href={`/?league=${param}`}>
+            {title}
+      </Link>
     </div>
   )
 }
