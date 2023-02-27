03f7868b0041a0763f62a0a2d370e0fb1544457b
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-27 11:32:50 -0500

    updates to nvabar component

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,9 +3,9 @@ import NavBarItem from "./NavBarItem";
 function NavBar() {
   return (
     <div className="flex justify-center bg-blue-200">
-        <NavBarItem title="NBA" param="nba"/>
-        <NavBarItem title="NFL" param="nfl"/>
-        <NavBarItem title="NHL" param="nhl"/>
+        <NavBarItem title="Standings" param="standings"/>
+        <NavBarItem title="Scores" param="scores"/>
+        <NavBarItem title="Statistics" param="statistics"/>
     </div>
   )
 };
